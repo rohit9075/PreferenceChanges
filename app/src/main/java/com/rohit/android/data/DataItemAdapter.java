0d0308d93faa3e0895f3b6a8dc1ba904daea79c3
Rohit <rohit9075@gmail.com>
2018-05-03 13:13:30 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/android/data/DataItemAdapter.java
@@ -0,0 +1,121 @@
+package com.rohit.android.data;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.preference.PreferenceManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rohit.android.data.model.DataItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHolder> {
+
+    public static final String ITEM_ID_KEY = "item_id_key";
+    public static final String ITEM_KEY = "item_key";
+    private List<DataItem> mItems;
+    private Context mContext;
+    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
+
+    public DataItemAdapter(Context context, List<DataItem> items) {
+        this.mContext = context;
+        this.mItems = items;
+    }
+
+    @Override
+    public DataItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // creating the object of the SharedPreferences class
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+
+        // shared preference change listner
+        preferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Log.d("prererences", "onSharedPreferenceChanged: changed");
+            }
+        };
+
+        // register the sharedpreferences listner to get the changes
+        settings.registerOnSharedPreferenceChangeListener(preferenceChangeListener);
+
+
+        // getting the boolean value form the preferences as per user selection
+        Boolean grid = settings.getBoolean(mContext.getString(R.string.pref_display_grid),false);
+
+        // getting the id of the layouts to inflate xml layout
+        int layoutId = grid ? R.layout.grid_item : R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        View itemView = inflater.inflate(layoutId, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataItemAdapter.ViewHolder holder, int position) {
+        final DataItem item = mItems.get(position);
+
+        try {
+            holder.tvName.setText(item.getItemName());
+            String imageFile = item.getImage();
+            InputStream inputStream = mContext.getAssets().open(imageFile);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            holder.imageView.setImageDrawable(d);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(mContext, "You selected " + item.getItemName(),
+//                        Toast.LENGTH_SHORT).show();
+//                String itemId = item.getItemId();
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra(ITEM_KEY, item);
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.mView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(mContext, "You long clicked " + item.getItemName(),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvName;
+        public ImageView imageView;
+        public View mView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvName = (TextView) itemView.findViewById(R.id.itemNameText);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            mView = itemView;
+        }
+    }
+}
